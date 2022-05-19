c6ad756e83df5c8ca7d93bd7e7da414400229aba
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-05-19 10:06:50 +0900

    Edit config.fish.

--- utils/fish/config.fish
+++ utils/fish/config.fish
@@ -1,6 +1,6 @@
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -g fish_prompt_pwd_dir_length 0
 
-if test (service docker status | awk '{print $4}') = "not";
+if test (echo (service docker status | awk '{print $4}') | sed 's/  *//g') = "not";
   sudo service docker start > /dev/null
 end
