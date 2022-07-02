22b67b5bb59b003ec46562c32c1b981d90c6530f
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-07-02 01:23:52 +0000

    Edit setup.sh to enable ide.sh.

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# --- fish ------
 mkdir -p ~/.config/fish/conf.d/
 cp ./fish/mytheme_colors.fish ~/.config/fish/conf.d/mytheme_colors.fish
 
@@ -8,6 +9,15 @@ cp ./fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 
 mkdir -p ~/.config/fish/
 cp ./fish/config.fish ~/.config/fish/config.fish
+# --- fish ------
 
+# --- nvim ------
 mkdir -p ~/.config/nvim/
 cp ./nvim/init.vim ~/.config/nvim/init.vim
+# --- nvim ------
+
+# --- tmux ------
+mkdir -p ~/bin/
+cp ./tmux/ide.sh ~/bin/ide.sh
+echo PATH="$PATH:~/bin"
+# --- tmux ------
